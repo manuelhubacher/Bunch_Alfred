6d210c680bd370d12f62a788c1b1f3c1b865192e
Manuel S. Hubacher <manuelhubi@students.unibe.ch>
2022-03-13 13:54:33 +0100

    fix: Show Open Bunches
    No longer shows an empty item when no bunches are open

--- bunch_open.py
+++ bunch_open.py
@@ -12,7 +12,8 @@ def make_json_path(p):
         "arg": p,
         }
 
-json_item = {}
-bunch_items = [x for x in items.split(', ') if x]
-json_item['items']=list(map(make_json_path, bunch_items))
-print(json.dumps(json_item))
+if len(items) > 2:
+    json_item = {}
+    bunch_items = [x for x in items.split(', ') if x]
+    json_item['items']=list(map(make_json_path, bunch_items))
+    print(json.dumps(json_item))
